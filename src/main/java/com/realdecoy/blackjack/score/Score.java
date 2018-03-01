97174f75f6549e488f6087396d57289737350f81
denisdbell <denisdbell@gmail.com>
2018-03-01 16:55:21 -0500

    Add Score class

---
+++ src/main/java/com/realdecoy/blackjack/score/Score.java
@@ -0,0 +1,22 @@
+/**
+* <h1>Score</h1>
+* Generates the scores based on the value of the cards
+* <p>
+* 
+* @author  Denis Bell
+* @version 1.0
+*/
+
+
+package com.realdecoy.blackjack.score;
+
+import com.realdecoy.blackjack.card.Card;
+import java.util.ArrayList;
+
+public class Score {
+
+    public static generateScoreFromCards(ArrayList<Card> cards){
+
+    }
+
+}
