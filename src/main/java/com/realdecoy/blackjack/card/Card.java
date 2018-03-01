b2b6bdc0bdae533e3e1b4264078de937d0bbbad3
denisdbell <denisdbell@gmail.com>
2018-03-01 15:24:33 -0500

    Add Card class

---
+++ src/main/java/com/realdecoy/blackjack/card/Card.java
@@ -0,0 +1,42 @@
+/**
+* <h1>Card Class</h1>
+* This class repesents the combination of a Suit and CardValue enums
+* <p>
+* 
+* @author  Denis Bell
+* @version 1.0
+*/
+
+package com.realdecoy.blackjack.card;
+
+public class Card
+{
+  private Suit suit;
+  private CardValue cardValue;
+ 
+  public Card (CardValue cardValue, Suit suit)
+  {
+    this.cardValue = cardValue;
+    this.suit = suit;
+  }
+ 
+  public Suit getSuit()
+  {
+    return suit;
+  }
+ 
+  public void setSuit(Suit suit)
+  {
+    this.suit = suit;
+  }
+ 
+  public CardValue getCardValue()
+  {
+    return cardValue;
+  }
+ 
+  public void setCardValue(CardValue cardValue)
+  {
+    this.cardValue = cardValue;
+  }
+}
