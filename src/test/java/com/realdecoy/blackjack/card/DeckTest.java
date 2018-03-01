5b1a94b64da641b321c620d6dca5e4b6ce08f2eb
denisdbell <denisdbell@gmail.com>
2018-03-01 16:42:07 -0500

    Add test_shuffle_should_sort_the_deck test

--- src/test/java/com/realdecoy/blackjack/card/DeckTest.java
+++ src/test/java/com/realdecoy/blackjack/card/DeckTest.java
@@ -10,27 +10,53 @@
 package com.realdecoy.blackjack.card;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import java.util.Collections;
+import java.util.ArrayList;
 import org.junit.*;
 
 
 public class DeckTest {
 
+    Integer TOTAL_DECK_SIZE = 52;
+
     Deck deck;
 
     @Before
     public void setUp(){
-        System.out.println("Running");
         deck = new Deck();
     }
 
     @Test
     public void test_initilize_should_populate_deck_with_52_cards() {
         
-        //Deck should be empty initially
+
+        //Deck should be empty initially i.e size of zero
         assertTrue(deck.getDeck().size() == 0);
 
         deck.initialize();
 
-        assertTrue(deck.getDeck().size() == 52);
+        //Should have a total size of 52
+        assertTrue(deck.getDeck().size() == TOTAL_DECK_SIZE);
+    }
+
+    @Test
+    public void test_shuffle_should_sort_the_deck(){
+       
+        deck.initialize();
+
+        ArrayList originalDeck = new ArrayList();
+
+        originalDeck.addAll(deck.getDeck());
+
+        deck.shuffle();
+
+        System.out.println(deck.getDeck() + " " + originalDeck);
+
+        assertTrue(deck.getDeck().size() == originalDeck.size());
+
+        assertTrue(!deck.getDeck().equals(originalDeck) );
+
     }
 }
