d32a97992114bcf0180bb6149d56082a485337a9
denisdbell <denisdbell@gmail.com>
2018-03-01 16:55:04 -0500

    Add getScore method

--- src/main/java/com/realdecoy/blackjack/player/Player.java
+++ src/main/java/com/realdecoy/blackjack/player/Player.java
@@ -6,16 +6,29 @@
 * @author  Denis Bell
 * @version 1.0
 */
+package com.realdecoy.blackjack.player;
+
+import com.realdecoy.blackjack.card.Card;
+
 public abstract class Player{
 
     String name;
     Integer score;
+    ArrayList<Card> cards;
 
     public String getName(){
         return this.name;
     }
 
     public void setName(String name){
+        this.name = name;
+    }
+
+      public Integer getScore(){
+        return this.score;
+    }
+
+    public void setScore(String name){
         this.name = name;
     }
 }
