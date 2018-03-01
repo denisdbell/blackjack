f4f3c33f9ecc48cd6db8d14a9244efd893799cf2
denisdbell <denisdbell@gmail.com>
2018-03-01 15:25:07 -0500

    Add Suit enum

---
+++ src/main/java/com/realdecoy/blackjack/card/Suit.java
@@ -0,0 +1,18 @@
+/**
+* <h1>Card Enum</h1>
+* Contains the suits for the cards
+* <p>
+* 
+* @author  Denis Bell
+* @version 1.0
+*/
+
+package com.realdecoy.blackjack.card;
+
+public enum Suit
+{
+  HEARTS,
+  SPADES,
+  CLUBS,
+  DIAMONDS;
+}
