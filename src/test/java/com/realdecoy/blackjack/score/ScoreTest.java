28469e4495a7314b4b20ea6077b2f12dd1480423
denisdbell <denisdbell@gmail.com>
2018-03-01 18:14:38 -0500

    Complete test cases for Score class

--- src/test/java/com/realdecoy/blackjack/score/ScoreTest.java
+++ src/test/java/com/realdecoy/blackjack/score/ScoreTest.java
@@ -16,25 +16,49 @@ import java.util.Collections;
 import java.util.ArrayList;
 import org.junit.*;
 import com.realdecoy.blackjack.score.Score;
+import com.realdecoy.blackjack.card.Card;
+import com.realdecoy.blackjack.card.CardValue.*;
+import com.realdecoy.blackjack.card.Suit;
+
+
 
 
 public class ScoreTest {
 
-    Integer TOTAL_DECK_SIZE = 52;
-
     Deck deck;
+
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+    Integer cardScore;
 
     @Before
     public void setUp(){
-        deck = new Deck();
+        deck = new Deck();  
+
+        cards = new ArrayList<Card>(){{
+		   add(new Card(CardValue.THREE,Suit.HEARTS));
+		   add(new Card(CardValue.QUEEN,Suit.SPADES));
+		   add(new Card(CardValue.KING,Suit.CLUBS));
+		}};
+
+        cardScore = 23; 
     }
 
     @Test
-    public void test_generateScoreFromCards_should_calculate_core() {
+    public void test_generateScoreFromCards_should_calculate_correct_score() {
         
         deck.initialize();
 
-        Score.generateScoreFromCards(deck.getDeck());
+        Integer score = Score.generateScoreFromCards(cards);
 
+        assertTrue(score == cardScore);
+    }
+
+    @Test
+    public void test_generateScoreFromCards_should_return_zero_if_empty_list_is_provided() {
+        
+        Integer score = Score.generateScoreFromCards(new ArrayList<Card>());
+
+        assertTrue(score == 0);
     }
 }
