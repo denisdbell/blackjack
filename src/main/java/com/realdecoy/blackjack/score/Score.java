7d263381327d336ed5b3b8b2e85bf58652ef95ce
denisdbell <denisdbell@gmail.com>
2018-03-01 17:33:34 -0500

    Add initial implementation of generateScoreFromCards method

--- src/main/java/com/realdecoy/blackjack/score/Score.java
+++ src/main/java/com/realdecoy/blackjack/score/Score.java
@@ -6,17 +6,26 @@
 * @author  Denis Bell
 * @version 1.0
 */
-
-
 package com.realdecoy.blackjack.score;
 
 import com.realdecoy.blackjack.card.Card;
 import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class Score {
 
-    public static generateScoreFromCards(ArrayList<Card> cards){
+    public static Integer generateScoreFromCards(ArrayList<Card> cards){
+        
+        Iterator cardIterator = cards.iterator();
+        
+        while (cardIterator.hasNext()){
 
+            Card aCard = (Card) cardIterator.next();
+            System.out.println(aCard.getCardValue() + " of " + aCard.getSuit());
+        }
+
+        return 0;
     }
 
 }
