75304285f79a87d1acefd22b14e62966a3b05ddb
denisdbell <denisdbell@gmail.com>
2018-03-02 08:41:30 -0500

    Add  functionality to run black jack application

--- src/main/java/com/realdecoy/blackjack/App.java
+++ src/main/java/com/realdecoy/blackjack/App.java
@@ -1,9 +1,28 @@
 package com.realdecoy.blackjack;
+import com.realdecoy.blackjack.game.BlackJack;
+import java.util.Scanner;
+import com.realdecoy.blackjack.util.ConsoleColor;
+
+
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        BlackJack blackJack = new BlackJack();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(ConsoleColor.ANSI_YELLOW + "***********************************************************************" + ConsoleColor.ANSI_RESET);
+        System.out.println(ConsoleColor.ANSI_YELLOW + "*************    Welcome to RealDecoy's Black Jack Game   *************" + ConsoleColor.ANSI_RESET);
+        System.out.println(ConsoleColor.ANSI_YELLOW + "*************             Author: Denis Bell              *************" + ConsoleColor.ANSI_RESET);
+        System.out.println(ConsoleColor.ANSI_YELLOW + "*************           Press any key to START!           *************" + ConsoleColor.ANSI_RESET);
+        System.out.println(ConsoleColor.ANSI_YELLOW + "***********************************************************************" + ConsoleColor.ANSI_RESET);
+
+        scan.nextLine();
+
+        while(blackJack.play()){
+
+        }
     }
     
 }
