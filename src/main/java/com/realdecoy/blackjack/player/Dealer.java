8115aa418c30bae9da60558999112812254b3c91
denisdbell <denisdbell@gmail.com>
2018-03-01 21:55:21 -0500

    Create Dealer class

---
+++ src/main/java/com/realdecoy/blackjack/player/Dealer.java
@@ -0,0 +1,13 @@
+/**
+* <h1>Dealer Player Class</h1>
+* Represents a Dealer in the BlackJack game. Inherits from Player Class 
+* <p>
+* 
+* @author  Denis Bell
+* @version 1.0
+*/
+package com.realdecoy.blackjack.player;
+
+public class Dealer extends Player{
+
+}
