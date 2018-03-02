1c7644115c66039b774f264526ddb29fb3ea5b0b
denisdbell <denisdbell@gmail.com>
2018-03-01 21:55:04 -0500

    Create BlackJackPlayer class

---
+++ src/main/java/com/realdecoy/blackjack/player/BlackJackPlayer.java
@@ -0,0 +1,13 @@
+/**
+* <h1>BlackJack Player Class</h1>
+* Represents a BlackJack Game player. Inherits from Player Class 
+* <p>
+* 
+* @author  Denis Bell
+* @version 1.0
+*/
+package com.realdecoy.blackjack.player;
+
+public class BlackJackPlayer extends Player{
+    
+}
